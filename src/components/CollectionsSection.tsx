ad530ef394305394e49a8061408283508d450058
pcremoremo-crypto <pcremoremo@gmail.com>
2025-11-03 12:22:02 -0500

    todos los combios unidos

--- src/components/CollectionsSection.tsx
+++ src/components/CollectionsSection.tsx
@@ -81,7 +81,7 @@ export const CollectionsSection = () => {
     fetch('/api/products')
       .then(res => res.json())
       .then(data => {
-        setProducts(data);
+        setProducts(data.products);
         setIsLoading(false);
       })
       .catch(err => {
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 export const Hero = () => {
   const scrollToProducts = () => {
-    const productsSection = document.getElementById('colecciones');
+    const productsSection = document.getElementById('productos');
     productsSection?.scrollIntoView({ behavior: 'smooth' });
   };
 
